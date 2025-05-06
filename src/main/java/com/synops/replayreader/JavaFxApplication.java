35fba93532896aa2488edb873e9886a5644652e8
synopss <synopss@outlook.com>
2025-05-06 19:01:19 +0200

    feat: adding "Report bug" menu item

--- src/main/java/com/synops/replayreader/JavaFxApplication.java
+++ src/main/java/com/synops/replayreader/JavaFxApplication.java
@@ -1,11 +1,15 @@
 package com.synops.replayreader;
 
 import com.synops.replayreader.core.StageReadyEvent;
+import java.util.Objects;
 import javafx.application.Application;
+import javafx.application.HostServices;
 import javafx.application.Platform;
 import javafx.stage.Stage;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 
 public class JavaFxApplication extends Application {
 
@@ -13,11 +17,14 @@ public class JavaFxApplication extends Application {
 
   @Override
   public void init() {
-    applicationContext = new SpringApplicationBuilder(ReplayReaderApplication.class).run();
+    applicationContext = new SpringApplicationBuilder(ReplayReaderApplication.class)
+        .initializers(getInitializer())
+        .run();
   }
 
   @Override
   public void start(Stage stage) {
+    Objects.requireNonNull(applicationContext);
     applicationContext.publishEvent(new StageReadyEvent(stage));
   }
 
@@ -25,5 +32,9 @@ public class JavaFxApplication extends Application {
   public void stop() {
     applicationContext.close();
     Platform.exit();
+  }
+
+  private ApplicationContextInitializer<GenericApplicationContext> getInitializer() {
+    return applicationContext -> applicationContext.registerBean(HostServices.class, this::getHostServices);
   }
 }
